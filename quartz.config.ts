0b2ed30c6ee850d408954feb10f88ae14b46c4c6
Cédric <88480529+cedbo26@users.noreply.github.com>
2025-07-25 04:25:44 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Facilo Rapido Interne",
+    pageTitle: "Facilo Rapido",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
